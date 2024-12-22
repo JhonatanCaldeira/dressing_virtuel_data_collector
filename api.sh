e1523d8e067817c675d6408b587094dc90d05297
JhonatanCaldeira <jhonatancaldeira@yahoo.com.br>
2024-12-22 12:15:00 +0100

    CI/CD

--- api.sh
+++ api.sh
@@ -10,9 +10,11 @@ if [ "$1" = "all" ]; then
         nohup uvicorn api.api_server_db:app --port $PG_API_PORT > logs/api/api_server_db.log 2>&1 &
         nohup uvicorn api.api_models:app --port $MODELS_API_PORT > logs/api/api_models.log 2>&1 &
         nohup uvicorn api.api_celery:app --port $CELERY_API_PORT > logs/api/api_celery.log 2>&1 &
+        nohup celery -A broker.tasks worker --loglevel=INFO > logs/api/celery.log 2>&1 &
     
     elif [ "$2" = "stop" ]; then
         pkill -f "uvicorn .*:app"
+        pkill -f "celery -A broker.tasks worker"
     fi
 else 
     if [ "$2" = "start" ]; then
